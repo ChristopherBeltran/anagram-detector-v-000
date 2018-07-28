06e482f38c7d83536ef1c092ddeedb4b88a9f53a
Christopher Beltran <christod.beltran@gmail.com>
2018-07-28 22:03:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,6 +14,11 @@ def match(list)
   grams = @gram.scan /\w/
   if letters.sort == grams.sort
     matches << word
+  end 
+  if matches.length == 0
+    return []
+  else 
+    return matches 
     
   
   
