21db74ffaf9c9613e181dd06c1aea0ef932055c9
Christopher Beltran <christod.beltran@gmail.com>
2018-07-28 19:26:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :gram
+    
+def initialize(gram)
+  @gram = gram
+end
+
+def match(list)
+  list.each do |word|
+  letters = word.split(" ")
+  
+  if letters.sort == gram.sort
+    binding.pry
+    return word
+  end
+end 
+  
+  
+end 
+end 
